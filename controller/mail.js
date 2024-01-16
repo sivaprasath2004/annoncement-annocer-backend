07ba9818f6d31b727575d894285f0fb4a3fc0d64
Sivaprasath <121082414+sivaprasath2004@users.noreply.github.com>
2024-01-16 09:55:51 +0530

    Update mail.js

--- controller/mail.js
+++ controller/mail.js
@@ -19,7 +19,12 @@ const receiver={
     subject:req.body.email.Subject,
     text:'tested successful',
     html:html,
-    mails,
+    attachments: [
+    {
+      filename:req.body.Attachment
+      content: 'Attachment',
+    },
+  ],
 }
 const sendMail=async(sender,receiver)=>{
     try{
